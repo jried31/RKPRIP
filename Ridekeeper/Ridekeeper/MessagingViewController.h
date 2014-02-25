b354338fdc46222d4b7b9cb5ff223d3d5dfef96c
CLICC User <cliccuser@EMS-L06.(none)>
2014-02-25 14:46:16 -0800

    new QuickBlox messaging

---
+++ Ridekeeper/Ridekeeper/MessagingViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagingViewController.h
+//  Ridekeeper
+//
+//  Created by CLICC User on 2/25/14.
+//  Copyright (c) 2014 Dennis Grijalva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessagingViewController : UIViewController
+
+@end
---
+++ Ridekeeper/Ridekeeper/MessagingViewController.m
@@ -0,0 +1,38 @@
+//
+//  MessagingViewController.m
+//  Ridekeeper
+//
+//  Created by CLICC User on 2/25/14.
+//  Copyright (c) 2014 Dennis Grijalva. All rights reserved.
+//
+
+#import "MessagingViewController.h"
+
+@interface MessagingViewController ()
+
+@end
+
+@implementation MessagingViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
