1a0a2ea060b85d55c5024cca45865858ed2cdd5a
Eduardo Urrego - 7145100075 <rosa@Rosas-MacBook-Pro.local>
2014-02-25 03:29:27 -0800

    cleaned up storyboard, test commit.

---
+++ Ridekeeper/settingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  settingsViewController.h
+//  Ridekeeper
+//
+//  Created by Eduardo Urrego - 7145100075 on 2/25/14.
+//  Copyright (c) 2014 Dennis Grijalva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface settingsViewController : UIViewController
+
+@end
---
+++ Ridekeeper/settingsViewController.m
@@ -0,0 +1,43 @@
+//
+//  settingsViewController.m
+//  Ridekeeper
+//
+//  Created by Eduardo Urrego - 7145100075 on 2/25/14.
+//  Copyright (c) 2014 Dennis Grijalva. All rights reserved.
+//
+
+#import "settingsViewController.h"
+
+@interface settingsViewController ()
+
+@end
+
+@implementation settingsViewController
+
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
