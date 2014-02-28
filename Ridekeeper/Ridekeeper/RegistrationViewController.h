a1e7d991e5c9d7ff9b90f219a83fdd9108bf5f4c
CLICC User <cliccuser@ems-l03.local>
2014-02-27 16:10:13 -0800

    messaging files

---
+++ Ridekeeper/Ridekeeper/RegistrationViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegistrationViewController.h
+//  Ridekeeper
+//
+//  Created by CLICC User on 2/27/14.
+//  Copyright (c) 2014 Dennis Grijalva. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RegistrationViewController : NSObject
+
+@end
