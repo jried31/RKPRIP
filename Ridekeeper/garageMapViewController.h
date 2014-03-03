cfb6702e323d7dd9c31434f29177d15fbefdb3d7
CLICC User <cliccuser@ems-l29.local>
2014-03-03 11:50:52 -0800

    updated main storyboard

---
+++ Ridekeeper/garageMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  garageMapViewController.h
+//  Ridekeeper
+//
+//  Created by CLICC User on 3/3/14.
+//  Copyright (c) 2014 Dennis Grijalva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface garageMapViewController : UIViewController
+
+@end
