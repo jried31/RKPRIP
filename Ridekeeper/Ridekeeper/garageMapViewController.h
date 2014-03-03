1a7e1ed5e8a0446b9e3a11ee18efee4aef1f9fb0
CLICC User <cliccuser@ems-l29.local>
2014-03-03 12:15:40 -0800

    updated storyboard

---
+++ Ridekeeper/Ridekeeper/garageMapViewController.h
@@ -0,0 +1,19 @@
+//
+//  garageMapViewController.h
+//  Ridekeeper
+//
+//  Created by CLICC User on 3/3/14.
+//  Copyright (c) 2014 Dennis Grijalva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+@interface garageMapViewController : UIViewController {
+    MKMapView *mapview;
+}
+
+@property (nonatomic,retain) IBOutlet MKMapView *mapview;
+
+-(IBAction)SetMap:(id)sender;
+
+@end
---
+++ Ridekeeper/Ridekeeper/garageMapViewController.m
@@ -0,0 +1,57 @@
+//
+//  garageMapViewController.m
+//  Ridekeeper
+//
+//  Created by CLICC User on 3/3/14.
+//  Copyright (c) 2014 Dennis Grijalva. All rights reserved.
+//
+
+#import "garageMapViewController.h"
+
+@interface garageMapViewController ()
+
+@end
+
+@implementation garageMapViewController
+
+@synthesize mapview;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)SetMap:(id)sender {
+    
+    switch (((UISegmentedControl *) sender).selectedSegmentIndex) {
+        case 0:
+            mapview.mapType = MKMapTypeStandard;
+            break;
+        case 1:
+            mapview.mapType = MKMapTypeSatellite;
+            break;
+        case 2:
+            mapview.mapType = MKMapTypeHybrid;
+            break;
+        default:
+            break;
+    }
+}
+
+@end
